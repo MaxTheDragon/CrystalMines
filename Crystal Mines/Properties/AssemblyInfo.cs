df0a1db3f09189f91a8d0c3af4322b40ba25a590
MaxTheDragon <MaxTheDragon@outlook.com>
2017-06-20 11:39:17 +0200

    Add project files.

---
+++ Crystal Mines/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Crystal Mines")]
+[assembly: AssemblyProduct("Crystal Mines")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("A 2D tile-based mining and puzzle game.")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("Copyright © MaxTheDragon 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("69ad261f-5ea0-4395-91a8-d627d9c2eb8c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
